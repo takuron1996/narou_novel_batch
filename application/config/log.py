f6d3298e988836ff1660f792e95c0d06b605b34a
taku ikegami <takuron1996@gmail.com>
2024-12-08 20:40:34 +0900

    loggerの設定を修正

--- application/config/log.py
+++ application/config/log.py
@@ -4,7 +4,6 @@ import tomllib
 from enum import Enum
 from logging import getLogger
 from logging.config import dictConfig
-from pathlib import Path
 
 
 class LoggerName(Enum):
@@ -22,20 +21,5 @@ def read_logger(filename):
     with open(filename, mode="rb") as file:
         dictConfig(tomllib.load(file).get("logging"))
 
-
-def prepare_logger(output_directory="target", conf_file="log.toml"):
-    """ロガーの事前準備
-
-    Args:
-        output_directory (str, optional): ログの出力先ディレクトリ
-        conf_file (str, optional): ログの設定ファイル（toml形式）. defualt: log.toml
-    """
-    output_path = Path(output_directory)
-    if not output_path.exists():
-        output_path.mkdir()
-
-    read_logger(conf_file)
-
-
-prepare_logger()
+read_logger("log.toml")
 console_logger = getLogger(LoggerName.CONSOLE.value)
