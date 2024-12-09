1b85961b7ccd5dcd6ebae0529ba12a7d61475f87
taku ikegami <takuron1996@gmail.com>
2024-12-10 02:56:39 +0900

    リファクタリング

--- application/repository/daily_get_ranking_repository.py
+++ application/repository/daily_get_ranking_repository.py
@@ -3,35 +3,32 @@ from pathlib import Path
 from dataclasses import asdict
 
 
+def get_sql_query(file_name):
+    """SQLファイルのパスからファイルの中身を取得"""
+    current_dir = Path(__file__).parent
+    file_path = current_dir / "sql" / "daily_get_ranking_repository" / file_name
+    with file_path.open("r") as file:
+        return file.read()
+
+
 def ranking_insert(session, narou_rank_data_list):
     """ならうランキングデータを投入"""
-    current_dir = Path(__file__).parent
-    file_path = (
-        current_dir
-        / "sql"
-        / "daily_get_ranking_repository"
-        / "ranking_insert_ncode_mapping.sql"
-    )
     params = tuple(map(asdict, narou_rank_data_list))
-    with file_path.open("r") as file:
-        sql_query = file.read()
-        session.execute(
-            text(sql_query).bindparams(ncode="ncode").bindparams(id="id"),
-            params,
-        )
-    file_path = (
-        current_dir
-        / "sql"
-        / "daily_get_ranking_repository"
-        / "ranking_insert_rank.sql"
+
+    # nocde_mappingの登録
+    sql_query = get_sql_query("ranking_insert_ncode_mapping.sql")
+    session.execute(
+        text(sql_query).bindparams(ncode="ncode").bindparams(id="id"),
+        params,
     )
-    with file_path.open("r") as file:
-        sql_query = file.read()
-        session.execute(
-            text(sql_query)
-            .bindparams(id="id")
-            .bindparams(rank="rank")
-            .bindparams(rank_date="rank_date"),
-            params,
-        )
-        session.commit()
+
+    # rankの登録
+    sql_query = get_sql_query("ranking_insert_rank.sql")
+    session.execute(
+        text(sql_query)
+        .bindparams(id="id")
+        .bindparams(rank="rank")
+        .bindparams(rank_date="rank_date"),
+        params,
+    )
+    session.commit()
