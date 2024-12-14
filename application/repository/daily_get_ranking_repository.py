fe9daa25b281b3b1eb920b5a3d815e9be7b6b957
taku ikegami <takuron1996@gmail.com>
2024-12-14 11:38:18 +0900

    DB登録失敗時にエラー処理を追加

--- application/repository/daily_get_ranking_repository.py
+++ application/repository/daily_get_ranking_repository.py
@@ -13,25 +13,26 @@ sub_dirs = ["sql", "daily_get_ranking_repository"]
 
 def ranking_insert(session, narou_rank_data_list):
     """ならうランキングデータを投入."""
-    params = tuple(map(asdict, narou_rank_data_list))
+    try:
+        params = tuple(map(asdict, narou_rank_data_list))
 
-    console_logger.debug("nocde_mappingの登録")
-    sql_query = get_sql_query(
-        "ranking_insert_ncode_mapping.sql", base_dir, sub_dirs
-    )
-    session.execute(
-        text(sql_query).bindparams(ncode="ncode").bindparams(id="id"),
-        params,
-    )
+        console_logger.debug("nocde_mappingの登録")
+        sql_query = get_sql_query(
+            "ranking_insert_ncode_mapping.sql", base_dir, sub_dirs
+        )
+        session.execute(
+            text(sql_query),
+            params,
+        )
 
-    console_logger.debug("rankの登録")
-    sql_query = get_sql_query("ranking_insert_rank.sql", base_dir, sub_dirs)
-    session.execute(
-        text(sql_query)
-        .bindparams(id="id")
-        .bindparams(rank="rank")
-        .bindparams(rank_date="rank_date")
-        .bindparams(rank_type="rank_type"),
-        params,
-    )
-    session.commit()
+        console_logger.debug("rankの登録")
+        sql_query = get_sql_query("ranking_insert_rank.sql", base_dir, sub_dirs)
+        session.execute(
+            text(sql_query),
+            params,
+        )
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        console_logger.error("ランキングデータの登録に失敗しました。")
+        raise e
