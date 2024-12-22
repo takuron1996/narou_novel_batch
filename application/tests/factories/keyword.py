a60c9d175df1aa916ff03fbe17746525c22dbe5a
taku ikegami <takuron1996@gmail.com>
2024-12-22 23:59:33 +0900

    factoryの初期値を追加

--- application/tests/factories/keyword.py
+++ application/tests/factories/keyword.py
@@ -18,3 +18,4 @@ class KeywordFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session_persistence = "commit"
 
     keyword_id = factory.LazyFunction(lambda: uuid.uuid4())
+    name = factory.Sequence(lambda n: f"name_{n}")
