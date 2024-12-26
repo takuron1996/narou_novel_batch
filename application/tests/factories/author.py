89e8ceeb8d90680b09bad74a1fcba9c158954720
taku ikegami <takuron1996@gmail.com>
2024-12-27 06:55:47 +0900

    ID設計に合わせてidを修正

--- application/tests/factories/author.py
+++ application/tests/factories/author.py
@@ -1,10 +1,8 @@
 """authorのファクトリー関連."""
 
-import uuid
-
 import factory
 
-from models.author import Author
+from models.author import Author, get_author_id
 
 
 class AuthorFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -17,6 +15,6 @@ class AuthorFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = None
         sqlalchemy_session_persistence = "commit"
 
-    author_id = factory.LazyFunction(lambda: uuid.uuid4())
+    author_id = factory.LazyFunction(lambda: get_author_id())
     userid = factory.Sequence(lambda n: n + 1)
     writer = factory.Sequence(lambda n: f"name_{n}")
