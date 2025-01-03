2728aac188dcf4e8cc1664629db3ea92a4d63d92
taku ikegami <takuron1996@gmail.com>
2025-01-03 21:58:52 +0900

    version3のflow処理を追加

--- application/batch/get_novel_data.py
+++ application/batch/get_novel_data.py
@@ -2,13 +2,14 @@
 
 import itertools
 
-from prefect import task
+from prefect import flow, task
 
 from apis.narou.narou_data import NarouData, NarouDataMapper
 from apis.narou.type import NarouOfType, NarouOrderType, OutType
 from apis.narou.urls import NarouURLBuilder
 from apis.request import request_get
 from batch.data import NcodeMappingData
+from common.datetime_util import jst_now
 from config.db import get_session
 from config.log import console_logger
 from models.author import get_author_id
@@ -27,13 +28,15 @@ def get_target_from_db() -> list[NcodeMappingData]:
     """取得対象をDBから取得."""
     console_logger.info("取得対象をDBから取得")
     # 1-1 ncode_mappingとnovelテーブルから取得対象を取得
-    console_logger.info("1-1 ncode_mappingとnovelテーブルから取得対象を取得")
+    console_logger.info(
+        "task-1-1 ncode_mappingとnovelテーブルから取得対象を取得"
+    )
     with get_session() as session:
         data_list = get_target_novel_data(session)
 
     # 1-2 取得したい小説のリストを作成
     # 1-3 return data_list
-    console_logger.info("1-2 取得したい小説のリストを作成")
+    console_logger.info("task-1-2 取得したい小説のリストを作成")
     return list(map(lambda x: NcodeMappingData(**x), data_list))
 
 
@@ -42,7 +45,7 @@ def fetch_novel_info(data_list: list[NcodeMappingData]):
     """小説情報を取得."""
     console_logger.info("小説情報を取得")
     # 2-1 data_listを整形
-    console_logger.info("2-1 data_listを整形")
+    console_logger.info("task-2-1 data_listを整形")
 
     ## data_listが空リストの場合はそのまま返却
     if not data_list:
@@ -53,7 +56,7 @@ def fetch_novel_info(data_list: list[NcodeMappingData]):
 
     # 2-2 なろうAPIから小説情報を取得
     ## t-u-n-w-bg-g-k-nt-ir-ibl-igl-izk-its-iti
-    console_logger.info("2-2 なろうAPIから小説情報を取得")
+    console_logger.info("task-2-2 なろうAPIから小説情報を取得")
     of_list = [
         NarouOfType.TITLE,
         NarouOfType.USERID,
@@ -76,9 +79,10 @@ def fetch_novel_info(data_list: list[NcodeMappingData]):
         .set_order(NarouOrderType.NCODE_DESC)
         .set_output_format(OutType.JSON)
         .set_of_list(of_list)
+        .set_limit(len(ncode_list))
         .build()
     )
-    console_logger.info(f"生成したURL: {url}")
+    console_logger.debug(f"生成したURL: {url}")
     response = request_get(url)
     if response is None:
         console_logger.error("レスポンスが200以外のため終了")
@@ -92,18 +96,21 @@ def fetch_novel_info(data_list: list[NcodeMappingData]):
 def insert_novel_into_db(insert_data_list: list[NarouData]):
     """小説情報をDBへ登録."""
     # 3.1 insert_data_listをDB登録用に整形
-    console_logger.info("3.1 insert_data_listをDB登録用に整形")
+    console_logger.info("task-3.1 insert_data_listをDB登録用に整形")
     insert_keyword_list = get_insert_keyword_list(insert_data_list)
     insert_author_novel_list = get_insert_author_novel_list(insert_data_list)
     insert_novel_keyword_list = get_insert_novel_keyword_list(insert_data_list)
 
     # 3.2 各テーブルにデータを登録
-    console_logger.info("3.2 各テーブルにデータを登録")
+    console_logger.info("task-3.2 各テーブルにデータを登録")
     with get_session() as session:
         insert_keyword(session, insert_keyword_list)
         insert_author(session, insert_author_novel_list)
         insert_novel(session, insert_author_novel_list)
         insert_novel_keywords(session, insert_novel_keyword_list)
+
+        # 全て登録された場合のみ適用
+        session.commit()
 
 
 def get_insert_keyword_list(insert_data_list: list[NarouData]):
@@ -153,3 +160,51 @@ def get_insert_novel_keyword_list(insert_data_list: list[NarouData]):
             novel_keyword_data["keyword"] = keyword
             insert_novel_keyword_list.append(novel_keyword_data)
     return insert_novel_keyword_list
+
+
+def generate_flow_run_name():
+    """実行時の名前を生成."""
+    date = jst_now().strftime("%Y%m%d")
+    return f"get_novel_data_{date}"
+
+
+def get_chunk_list(input_list: list, chunk_size: int):
+    """指定されたサイズでリストを分割する関数.
+
+    Parameters:
+        input_list (list): 分割するリスト.
+        chunk_size (int): 各チャンクのサイズ .
+
+    Returns:
+        list: 分割されたリストのリスト。
+    """
+    return [
+        input_list[i : i + chunk_size]
+        for i in range(0, len(input_list), chunk_size)
+    ]
+
+
+@flow(flow_run_name=generate_flow_run_name)
+def get_novel_data():
+    """小説情報取得バッチ."""
+    # 1-1 get_target_from_dbを実行
+    # 1回だけ実行
+    # data_listが空の場合は終了
+    console_logger.info("flow-1-1 get_target_from_dbを実行")
+    data_list = get_target_from_db()
+    if not data_list:
+        console_logger.info("対象のデータが存在しないため、処理を終了")
+        return
+
+    # 1-2 fetch_novel_infoを実行
+    # 500件ずつ区切って並列実行
+    # (APIの最大取得件数が500件なため)
+    console_logger.info("flow-1-2 fetch_novel_infoを実行")
+    chunke_data_list = get_chunk_list(data_list, 500)
+    fetch_data_list = fetch_novel_info.map(chunke_data_list).result()
+
+    # 1-3 insert_novel_into_dbを実行
+    # データをまとめて1回だけ実行
+    console_logger.info("flow-1-3 insert_novel_into_dbを実行")
+    insert_data_list = list(itertools.chain.from_iterable(fetch_data_list))
+    insert_novel_into_db(insert_data_list)
