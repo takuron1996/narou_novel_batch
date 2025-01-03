12e6d3ada669e2d6fd1dd7a6d3dae3656f4ee322
taku ikegami <takuron1996@gmail.com>
2025-01-03 21:59:19 +0900

    毎日23:30に実行するように設定

--- application/main.py
+++ application/main.py
@@ -2,9 +2,11 @@
 
 from prefect import serve
 
+from batch.get_novel_data import get_novel_data
 from batch.get_ranking import get_ranking
 
 if __name__ == "__main__":
+    # get_rankingの設定
     daily_get_ranking = get_ranking.to_deployment(
         name="daily_get_ranking",
         cron="0 14 * * *",
@@ -25,9 +27,18 @@ if __name__ == "__main__":
         cron="0 14 1 1,4,7,10 *",
         parameters={"rank_type": "q"},
     )
+
+    # get_novel_dataの設定
+    daily_get_novel_data = get_novel_data.to_deployment(
+        name="daily_get_novel_data",
+        cron="30 14 * * *",
+    )
+
+    # 起動
     serve(
         daily_get_ranking,
         weekly_get_ranking,
         monthly_get_ranking,
         quarterly_get_ranking,
+        daily_get_novel_data,
     )
